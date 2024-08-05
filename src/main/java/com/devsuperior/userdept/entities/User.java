8f570a1832f3321c2e0a9dc94fbe18c950b921e5
Rafael-Benicio <jerobenicio@gmail.com>
2024-08-04 21:21:37 -0300

    primeiro projeto java

---
+++ src/main/java/com/devsuperior/userdept/entities/User.java
@@ -0,0 +1,51 @@
+package com.devsuperior.userdept.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public User(){}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+}
